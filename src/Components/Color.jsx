b9bd823f08e893a6563cc102cecd12b129a85c53
abhishek <abhishek-ks1719@gmail.com>
2024-11-16 18:55:08 +0530

    color updating function changed

--- src/Components/Color.jsx
+++ src/Components/Color.jsx
@@ -12,12 +12,30 @@ const Color = ({color}) => {
         
         
         try {
-            const updatedNote = notes.map((note)=>
-              (note.$id===selectedNote.$id)?
-            {...note,colors:JSON.stringify(color)}:
-            note)
-            setNotes(updatedNote)
-            db.notes.update(selectedNote.$id,{colors:JSON.stringify(color)})
+            // const updatedNote = notes.map((note)=>
+            //   (note.$id===selectedNote.$id)?
+            // {...note,colors:JSON.stringify(color)}:
+            // note)
+            // setNotes(updatedNote)
+            // db.notes.update(selectedNote.$id,{colors:JSON.stringify(color)})
+
+            const currentNoteIndex = notes.findIndex(
+              (note) => note.$id === selectedNote.$id
+          );
+   
+          const updatedNote = {
+              ...notes[currentNoteIndex],
+              colors: JSON.stringify(color),
+          };
+   
+          const newNotes = [...notes];
+          newNotes[currentNoteIndex] = updatedNote;
+          setNotes(newNotes);
+   
+          db.notes.update(selectedNote.$id, {
+              colors: JSON.stringify(color),
+          });
+
         } catch (error) {
             alert("you must select Note")
         }
